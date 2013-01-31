05c98aaa0ae2d104414dc54415c1b98a43e2315e
tommy lin <yu_dady@yahoo.com.tw>
2013-01-31 10:26:46 +0800

    log4j

--- src/com/charitybuzz/controller/IndexController.java
+++ src/com/charitybuzz/controller/IndexController.java
@@ -1,19 +1,24 @@
 package com.charitybuzz.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 
 @Controller
-@RequestMapping(value = "/index")
+@RequestMapping(value = "/index.do")
 public class IndexController {
 	
+	/** logger. */
+	private Logger log = LoggerFactory.getLogger(IndexController.class);
 	
-	@RequestMapping(method = RequestMethod.GET)
+	//@RequestMapping(method = RequestMethod.GET)
+	@RequestMapping()
 	public String index(ModelMap model) {
-
+		log.debug("[LOG]log IndexController.index");
+		System.out.println("[LOG]System IndexController.index");
 		return "index";
 				
 	}

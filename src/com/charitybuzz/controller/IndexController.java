aded744bf389c93351c1c43492b5a378a1d7b043
tommy lin <yu_dady@yahoo.com.tw>
2013-01-31 10:14:13 +0800

    index.jsp

--- src/com/charitybuzz/controller/IndexController.java
+++ src/com/charitybuzz/controller/IndexController.java
@@ -1,5 +1,21 @@
 package com.charitybuzz.controller;
 
-public class IndexController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+
+@Controller
+@RequestMapping(value = "/index")
+public class IndexController {
+	
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String index(ModelMap model) {
+
+		return "index";
+				
+	}
+	
 }

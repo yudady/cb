fadfd3f99781c84749e52ff1957ddb7d4f6cb1cc
tommy lin <yu_dady@yahoo.com.tw>
2013-02-01 15:00:30 +0800

    categoryDao

---
+++ src/com/charitybuzz/dao/CategoryDao.java
@@ -0,0 +1,40 @@
+package com.charitybuzz.dao;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.charitybuzz.common.dao.QueryList;
+import com.charitybuzz.domain.Category;
+
+public class CategoryDao extends BaseDao<Category> {
+
+	/** logger. */
+	protected Logger log = LoggerFactory.getLogger(this.getClass());
+
+	public List<Category> findAll() {
+		String sql = "select * from category ";
+		return this.findAll(sql, new QueryList<Category>() {
+			@Override
+			public void doPreparedStatement() throws SQLException {
+			}
+
+			@Override
+			public List<Category> doResultSet() throws SQLException {
+				List<Category> categoryList = new ArrayList<Category>();
+				while (rs.next()) {
+					Category it = new Category();
+					it.setId(rs.getLong("id"));
+					it.setName(rs.getString("name"));
+					categoryList.add(it);
+				}
+				return categoryList;
+			}
+
+		});
+	}
+
+}
--- src/com/charitybuzz/service/CategoryService.java
+++ src/com/charitybuzz/service/CategoryService.java
@@ -1,20 +1,20 @@
 package com.charitybuzz.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import com.charitybuzz.dao.CategoryDao;
 import com.charitybuzz.domain.Category;
+
 public class CategoryService {
 
+	private CategoryDao categoryDao;
+
+	public void setCategoryDao(CategoryDao categoryDao) {
+		this.categoryDao = categoryDao;
+	}
+
 	public List<Category> findAll() {
-		List<Category> categories = new ArrayList<Category>();
-		categories.add(new Category(1L, "11111"));
-		categories.add(new Category(2L, "22222"));
-		categories.add(new Category(1L, "33333"));
-		categories.add(new Category(1L, "44444"));
-		categories.add(new Category(1L, "55555"));
-		
-		return categories;
+		return categoryDao.findAll();
 	}
 
 }

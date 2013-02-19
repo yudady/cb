9fc3eb78a69ac4e49790871f4d8fa325c1963531
tommy lin <yu_dady@yahoo.com.tw>
2013-02-19 15:01:59 +0800

    index

--- WebContent/js/base.js
+++ WebContent/js/base.js
@@ -1,18 +1,10 @@
 ;$(function() {
-	$.log('base js');
-	$("img.lazy").lazyload( { effect : "slideDown" } );
+	//$.log('base js');
+	
+	
+	//$("img.lazy").lazyload( { effect : "slideDown" } );
 	$("div").addClass('ui-corner-all');
 	
-	
-	
-	$("#loginFormBtn").click(function(){
-		$("#url").val(window.location.href);
-		$("#loginForm").submit();
-	});
-	$("#loginOutFormBtn").click(function(){
-		$("#url").val(window.location.href);
-		$("#loginOutForm").submit();
-	});
 });
 
 
